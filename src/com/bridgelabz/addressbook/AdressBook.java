0012406594018928efac02727f2122090bf623d1
sujit <sujitbokade2014@gmail.com>
2022-07-17 22:19:04 +0530

    Create Contacts

--- src/com/bridgelabz/addressbook/AdressBook.java
+++ src/com/bridgelabz/addressbook/AdressBook.java
@@ -1,7 +1,44 @@
 package com.bridgelabz.addressbook;
 
+
+class Contact
+{
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String mobileNumber;
+    String email;
+
+    Contact(String firstName, String lastName,String address,String city,String state,String zip,String mobileNumer,String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.mobileNumber = mobileNumer;
+        this.email  = email;
+    }
+    public void contacts()
+    {
+        System.out.println("First Name: "+firstName);
+        System.out.println("Last Name: "+lastName);
+        System.out.println("Address: "+address);
+        System.out.println("City: "+city);
+        System.out.println("State: "+state);
+        System.out.println("Zip Code: "+zip);
+        System.out.println("Mobile Number: "+mobileNumber);
+        System.out.println("Email Id: "+email);
+    }
+}
 public class AdressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System Program!");
+        Contact cont=new Contact("Sujit","Bokade","Shinde Plot","Barshi","Maharashtra","413411","9130754248","sujitbokade2014@gmail.com");
+        cont.contacts();
     }
 }
